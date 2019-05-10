7d81e216cfaddd500121a3f0cb2c1ce378d0e966
Lubna Naqvi <lubnanaqvi11@gmail.com>
2019-05-10 08:29:51 -0400

    Updates

---
+++ precache-manifest.14b0179edaf7db585de449c3109df30b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "337340eebcf96fe14db60a8cc2338bf2",
+    "url": "/portfolio19/static/media/web.337340ee.jpg"
+  },
+  {
+    "revision": "3de1187dce02863474168276fdf63acd",
+    "url": "/portfolio19/static/media/squares.3de1187d.png"
+  },
+  {
+    "revision": "2f1871071c64e58080ae",
+    "url": "/portfolio19/static/js/runtime~main.21331653.js"
+  },
+  {
+    "revision": "e9965e079adfd135f2a5",
+    "url": "/portfolio19/static/js/main.41b0284e.chunk.js"
+  },
+  {
+    "revision": "87513ea56f42afaf601c",
+    "url": "/portfolio19/static/js/2.849fef8f.chunk.js"
+  },
+  {
+    "revision": "e9965e079adfd135f2a5",
+    "url": "/portfolio19/static/css/main.bae2ea66.chunk.css"
+  },
+  {
+    "revision": "51d0d8966bff9f01402dc6dfda270540",
+    "url": "/portfolio19/index.html"
+  }
+];
